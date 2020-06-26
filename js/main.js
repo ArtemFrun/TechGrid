4eacb2ccae21c2a32531c7cacedab459405c613c
ArtemFrun <tjomka777@gmail.com>
2020-06-26 16:22:12 +0300

    block select

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(window).load(function() {
+    $(".before-after").twentytwenty({
+        before_label: 'January 2017', // Set a custom before label
+        after_label: 'March 2017' // Set a custom after label
+    });
+    $('.before-slider').slick({
+        draggable:false,
+        dots: true,
+        dotsClass: 'before-slider__dots',   
+        prevArrow: $('.arrow-left'),
+        nextArrow: $('.arrow-right')
+    });
+
+    /*Select*/
+    $('.select_checked').on('click', function(){
+        $('.select__dropdown').toggleClass('select__dropdown_open');
+    });
+    $('.select__option').on('click', function(){
+        var value = $(this).attr('data-value');
+        $('#select-type').val(value);
+        $('select_checked').text(value);
+        $('.select__dropdown').toggleClass('select__dropdown_open');
+    });
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top -200 + "px"});
+        return false;
+    });
+    });
